1ab7361ed94bcdedf00d8e58e580398328e0efe2
a17amama <37793130+a17amama@users.noreply.github.com>
2020-03-13 13:40:45 +0100

    Changed everything
    Drop-down is broken. It does not close dropwdown window  when clicking outside

--- js.js
+++ js.js
@@ -1,15 +1,29 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function firstDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
   // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
+  window.addEventListener("click", function(e) {
     if (!e.target.matches('.dropbtn')) {
     var myDropdown = document.getElementById("myDropdown");
       if (myDropdown.classList.contains('show')) {
         myDropdown.classList.remove('show');
       }
     }
+  });
+
+  function secondDropdown() {
+    document.getElementById("myDropdown2").classList.toggle("show");
   }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.addEventListener("onclick", function(hallo) {
+    if (!hallo.target.matches('.dropbtn')) {
+    var myDropdown2 = document.getElementById("myDropdown2");
+      if (myDropdown2.classList.contains('show')) {
+        myDropdown2.classList.remove('show');
+      }
+    }
+  });
