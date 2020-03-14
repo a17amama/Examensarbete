f027942ead4a3428a6cbf9656f40c33416504ccc
a17amama <37793130+a17amama@users.noreply.github.com>
2020-03-14 12:16:40 +0100

    Clicking outside broken

--- js.js
+++ js.js
@@ -1,29 +1,19 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function firstDropdown() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function firstDropdown(dropdowns) {
+ 
+    document.getElementById(dropdowns).classList.toggle("show");
   }
   
   // Close the dropdown if the user clicks outside of it
-  window.addEventListener("click", function(e) {
+  window.addEventListener ("click", function(e) {
     if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
+    var myDropdown = document.getElementsByClassName("dropdown-content");
+      if (myDropdown.classList.contains("show")) {
+        myDropdown.classList.remove("show");
       }
     }
   });
 
-  function secondDropdown() {
-    document.getElementById("myDropdown2").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.addEventListener("onclick", function(hallo) {
-    if (!hallo.target.matches('.dropbtn')) {
-    var myDropdown2 = document.getElementById("myDropdown2");
-      if (myDropdown2.classList.contains('show')) {
-        myDropdown2.classList.remove('show');
-      }
-    }
-  });
+  console.log(contains);
+
