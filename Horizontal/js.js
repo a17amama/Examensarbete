ea2d66deec9709bc09ba2351d878332ab372c78a
a17amama <37793130+a17amama@users.noreply.github.com>
2020-03-20 18:49:04 +0100

    styling finished

---
+++ Horizontal/js.js
@@ -0,0 +1,16 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function firstDropdown(dropdowns) {
+ 
+    document.getElementById(dropdowns).classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.addEventListener ("click", function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementsByClassName("dropdown-content");
+      if (myDropdown.classList.contains("show")) {
+        myDropdown.classList.remove("show");
+      }
+    }
+  });
