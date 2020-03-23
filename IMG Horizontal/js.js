27a2b8fcfa4d2cba79c6291f97937e7d4a8b93bd
a17amama <37793130+a17amama@users.noreply.github.com>
2020-03-23 12:51:29 +0100

    Update js.js

--- IMG Horizontal/js.js
+++ IMG Horizontal/js.js
@@ -1,18 +1,19 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function firstDropdown(dropdowns) {
- 
     document.getElementById(dropdowns).classList.toggle("show");
   }
   
   // Close the dropdown if the user clicks outside of it
   window.addEventListener ("click", function(e) {
     if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementsByClassName("dropdown-content");
-      if (myDropdown.classList.contains("show")) {
-        myDropdown.classList.remove("show");
+    const myDropdownList = document.getElementsByClassName("dropdown-content");
+    for( let i=0; i < myDropdownList.length;i++ ) {
+     const item = myDropdownList[i];
+      if (item.classList.contains("show")) {
+        item.classList.remove("show");
       }
     }
-  });
+  }});
 
- 
+   
