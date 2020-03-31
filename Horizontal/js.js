4f2344fd8ef579dbd2517aa6369149233874f858
a17amama <37793130+a17amama@users.noreply.github.com>
2020-03-31 21:13:35 +0200

    fixed menu bar

--- Horizontal/js.js
+++ Horizontal/js.js
@@ -1,16 +1,24 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function firstDropdown(dropdowns) {
- 
+  const myDropdownList = document.getElementsByClassName("dropdown-content");
+  for( let i=0; i < myDropdownList.length;i++ ) {
+   const item = myDropdownList[i];
+    if (item.classList.contains("show")) {
+      item.classList.remove("show");
+    }
+  }
     document.getElementById(dropdowns).classList.toggle("show");
   }
   
   // Close the dropdown if the user clicks outside of it
   window.addEventListener ("click", function(e) {
     if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementsByClassName("dropdown-content");
-      if (myDropdown.classList.contains("show")) {
-        myDropdown.classList.remove("show");
+    const myDropdownList = document.getElementsByClassName("dropdown-content");
+    for( let i=0; i < myDropdownList.length;i++ ) {
+     const item = myDropdownList[i];
+      if (item.classList.contains("show")) {
+        item.classList.remove("show");
       }
     }
-  });
+  }});
