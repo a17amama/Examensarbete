bb79fc2bedad34fa4cc282d3adf0268f2d78988d
a17amama <37793130+a17amama@users.noreply.github.com>
2020-03-31 15:42:15 +0200

    fixed menu

--- IMG Horizontal/js.js
+++ IMG Horizontal/js.js
@@ -1,6 +1,13 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function firstDropdown(dropdowns) {
+  const myDropdownList = document.getElementsByClassName("dropdown-content");
+  for( let i=0; i < myDropdownList.length;i++ ) {
+   const item = myDropdownList[i];
+    if (item.classList.contains("show")) {
+      item.classList.remove("show");
+    }
+  }
     document.getElementById(dropdowns).classList.toggle("show");
   }
   
