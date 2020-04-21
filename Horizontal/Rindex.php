a35b9b7625dcac5be92834bd8f53a0192b3c0b8c
a17amama <37793130+a17amama@users.noreply.github.com>
2020-04-21 23:33:36 +0200

    Finished

--- Horizontal/Rindex.php
+++ Horizontal/Rindex.php
@@ -19,72 +19,72 @@
       <!--Menu, with dropdowns and images in menu-->
         <div class="navbar" class="nav">
           <div class="logo">DØ<span style="color: rgb(248, 232, 8);">Z</span>E</div>
-            <a class="menu-link" href="#">Home </a>
+            <a class="menu-link log" href="#">Home </a>
 
             <div class="dropdown">
-            <button class="dropbtn" onclick="firstDropdown('myDropdown')">Bottoms </button>
+            <button class="dropbtn log" onclick="firstDropdown('myDropdown')">Bottoms </button>
             <div class="dropdown-content" id="myDropdown">
-              <a href="horTextHolder.php" class="log"> Pants </a>
-              <a href="horTextHolder.php"> Shorts </a>
-              <a href="horTextSkirt.php"> Skirts</a>
+              <a class="log" href="horTextHolder.php"> Pants </a>
+              <a class="log" href="horTextHolder.php"> Shorts </a>
+              <a class="log" href="horTextSkirt.php"> Skirts</a>
             </div>
             </div> 
 
             <div class="dropdown">
-                <button class="dropbtn" onclick="firstDropdown('myDropdown2')"> Tops </button>
+                <button class="dropbtn log" onclick="firstDropdown('myDropdown2')"> Tops </button>
                 <div class="dropdown-content" id="myDropdown2">
-                  <a href="horTextTshirt.php">T-shirts</a>
-                  <a href="horTextHolder.php">Tank tops</a>
-                  <a href="horTextHoodie.php">Hoodie</a>
-                  <a href="horTextHolder.php">Jacket</a>
-                  <a href="horTextHolder.php">Coat</a>
+                  <a class="log" href="horTextTshirt.php">T-shirts</a>
+                  <a class="log" href="horTextHolder.php">Tank tops</a>
+                  <a class="log" href="horTextHoodie.php">Hoodie</a>
+                  <a class="log" href="horTextHolder.php">Jacket</a>
+                  <a class="log" href="horTextHolder.php">Coat</a>
                 </div>               
             </div> 
 
             <div class="dropdown">
-              <button class="dropbtn" onclick="firstDropdown('myDropdown3')"> Swimwear </button>
+              <button class="dropbtn log" onclick="firstDropdown('myDropdown3')"> Swimwear </button>
               <div class="dropdown-content" id="myDropdown3">
-                <a href="horTextHolder.php">Bikini</a>
-                <a href="horTextHolder.php">Swimsuit</a>
-                <a href="horTextHolder.php">Shorts</a>
+                <a class="log" href="horTextHolder.php">Bikini</a>
+                <a class="log" href="horTextHolder.php">Swimsuit</a>
+                <a class="log" href="horTextHolder.php">Shorts</a>
               </div>               
           </div> 
 
           <div class="dropdown">
-            <button class="dropbtn" onclick="firstDropdown('myDropdown4')"> Underwear </button>
+            <button class="dropbtn log" onclick="firstDropdown('myDropdown4')"> Underwear </button>
             <div class="dropdown-content" id="myDropdown4">
-              <a href="horTextHolder.php">Bras</a>
-              <a href="horTextHolder.php">Panties</a>
-              <a href="horTextHolder.php">Boxers</a>
+              <a class="log" href="horTextHolder.php">Bras</a>
+              <a class="log" href="horTextHolder.php">Panties</a>
+              <a class="log" href="horTextHolder.php">Boxers</a>
             </div>               
           </div> 
 
 
 
         <div class="dropdown">
-          <button class="dropbtn" onclick="firstDropdown('myDropdown5')"> Accessories </button>
+          <button class="dropbtn log" onclick="firstDropdown('myDropdown5')"> Accessories </button>
           <div class="dropdown-content" id="myDropdown5">
-            <a href="horTextBag.php">Bags</a>
-            <a href="horTextHolder.php">Jewelry</a>
-            <a href="horTextHolder.php">Hats</a>
-            <a href="horTextHolder.php">Glasses</a>           
+            <a class="log" href="horTextBag.php">Bags</a>
+            <a class="log" href="horTextHolder.php">Jewelry</a>
+            <a class="log" href="horTextHolder.php">Hats</a>
+            <a class="log" href="horTextHolder.php">Glasses</a>           
           </div>               
         </div> 
 
         <div class="dropdown">
-        <button class="dropbtn" onclick="firstDropdown('myDropdown6')"> Shoes </button>
+        <button class="dropbtn log" onclick="firstDropdown('myDropdown6')"> Shoes </button>
         <div class="dropdown-content" id="myDropdown6">
-          <a href="horTextHolder.php">Sneakers</a>
-          <a href="horTextHeels.php">Heels</a>
-          <a href="horTextHolder.php">Sandals</a>
+          <a class="log" href="horTextHolder.php">Sneakers</a>
+          <a class="log" href="horTextHeels.php">Heels</a>
+          <a class="log" href="horTextHolder.php">Sandals</a>
         </div>               
         </div> 
 
     <div class="dropdown">
-      <button class="dropbtn" onclick="firstDropdown('myDropdown7')"> Overalls & Dresses </button>
+      <button class="dropbtn log" onclick="firstDropdown('myDropdown7')"> Overalls & Dresses </button>
       <div class="dropdown-content" id="myDropdown7">
-        <a href="horTextDress.php">Dresses</a>
-        <a href="horTextHolder.php">Overalls</a>
+        <a class="log" href="horTextDress.php">Dresses</a>
+        <a class="log" href="horTextHolder.php">Overalls</a>
       </div>               
   </div> 
 </div>
@@ -96,7 +96,7 @@
           
           <!--License ball at bottom right-->
           <div id="ball">
-            <p id="licenses">Licenses</p>
+            <p id="licenses" class="log">Licenses</p>
         </div>  
           
           
